a6bca6a6ba5b9e3d4536135a1cd4c4f40e2ac775
Sa_Ko <56673835+sam-k0@users.noreply.github.com>
2021-07-29 23:17:49 +0200

    Initial D
    a

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+
+
+#include <Windows.h>
+#include <stdio.h>
+#include <string>
+
+#define GMEXPORT extern "C" __declspec(dllexport)
+
+std::string GMgetWindowName() {
+    TCHAR title[500];
+
+    GetWindowText(GetForegroundWindow(), title, 500);
+
+    std::string titlestr = title;
+    return titlestr;
+}
