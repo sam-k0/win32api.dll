0fbaf590bb0af35a0f0c253335910d04415fd61e
Sa_Ko <56673835+sam-k0@users.noreply.github.com>
2021-07-31 10:26:36 +0200

    Added checkWindowName / Fixed getWindowNameC

--- main.cpp
+++ main.cpp
@@ -7,11 +7,21 @@
 
 #define GMEXPORT extern "C" __declspec(dllexport)
 
-GMEXPORT std::string GMgetWindowName() {
-    TCHAR title[500];
+GMEXPORT int checkWindowName(const char* windowName) {
+    // get handle to the searched window name
+    HWND winHandle = FindWindow(NULL,windowName);
+    // check if handle is null...
+    if(winHandle == NULL)
+    {
+        return -1;
+    }
+    return 1;
+}
 
+GMEXPORT char* getWindowNameC() {
+    char title[500]; // get the
     GetWindowText(GetForegroundWindow(), title, 500);
-
-    std::string titlestr = title;
-    return titlestr;
+    // typecast to char
+    char* titleptr = title;
+    return titleptr;
 }
