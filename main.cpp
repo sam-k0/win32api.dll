075f68fb1e0f192cdb987df55f7d8e2db1006fca
Sa_Ko <56673835+sam-k0@users.noreply.github.com>
2021-08-02 15:26:59 +0200

    Finishing / bug fixing

--- main.cpp
+++ main.cpp
@@ -24,20 +24,29 @@ HWND resolveHandle(const char* _passedHandle)
 
 //char* returnValPtr = &returnVal[0]; // Convert from string to char*
 
-GMEXPORT int checkWindowName(const char* windowName) {
-    // Checks if a window with a certain title exists
+GMEXPORT double initTest(double Parameter) // Geht / testfunktion
+{
+    return Parameter*2;
+}
 
+GMEXPORT double checkWindowName(const char* windowName, double returnValue) // geht / returns 1 yes 0 no
+{
+    // Checks if a window with a certain title exists
+    returnValue = 0;
     // get handle to the searched window name
     HWND winHandle = FindWindow(NULL,windowName);
     // check if handle is null...
     if(winHandle == NULL)
     {
-        return -1;
+        returnValue = 0;
+        return returnValue;
     }
-    return 1;
+    returnValue = 1;
+    return returnValue;
 }
 
-GMEXPORT char* getWindowNameC() {
+GMEXPORT char* getWindowNameC()  // Geht sicher / Returns string
+{
     // Shows the title of the currently focused window
     char title[500]; // get the
     GetWindowText(GetForegroundWindow(), title, 500);
@@ -46,8 +55,8 @@ GMEXPORT char* getWindowNameC() {
     return titleptr;
 }
 
-GMEXPORT int showMessageboxOk(const char* passedHandle, const char* headline, const char* bodytext)
-{
+GMEXPORT double showMessageboxOk(const char* passedHandle, const char* headline, const char* bodytext)
+{   // Not sure about return.. Wahrschreinlich random bullshit
     // Takes game maker window handle, headline, body text
     // returns whatever the msgbox id is.
     int msgboxID = MessageBox(
@@ -57,16 +66,14 @@ GMEXPORT int showMessageboxOk(const char* passedHandle, const char* headline, co
         MB_ICONINFORMATION | MB_OK | MB_DEFBUTTON2
     );
 
-    return msgboxID;
+    return (double)msgboxID;
 }
 
 
-GMEXPORT int showMessageboxTest(const char* passedHandle)
+GMEXPORT int showMessageboxTest(const char* passedHandle) // Geht / Testfunktion
 {
     // Takes game maker window handle, headline, body text
     // returns whatever the msgbox id is.
-
-
     int msgboxID = MessageBox(
         resolveHandle(passedHandle),
         "Is this a body?",
@@ -77,10 +84,10 @@ GMEXPORT int showMessageboxTest(const char* passedHandle)
     return msgboxID;
 }
 
-GMEXPORT int showMessageboxCTC(const char* passedHandle, const char* headline, const char* bodytext)
-{
+GMEXPORT double showMessageboxCTC(const char* passedHandle, const char* headline, const char* bodytext, double returnVal) // Geht / Janky
+{   // Operational / tut was sie soll wenn man glueck hat
     // this will show a messagebox with a continue, try again and cancel button.
-    int returnVal = 0; // Normalized button output value 0-2
+    returnVal = 0; // Normalized button output value 0-2
     int msgboxID = MessageBox(
         resolveHandle(passedHandle),
         bodytext,
@@ -109,9 +116,9 @@ GMEXPORT int showMessageboxCTC(const char* passedHandle, const char* headline, c
 }
 
 
-GMEXPORT int showMessageboxYN(const char* passedHandle, const char* headline, const char* bodytext)
+GMEXPORT double showMessageboxYN(const char* passedHandle, const char* headline, const char* bodytext, double returnVal)// geht
 {   // shows a messagebox with yes and no button, returns 0 for no and 1 for yes
-    int returnVal = 0;
+    returnVal = 0;
     int msgboxID = MessageBox(
         resolveHandle(passedHandle),
         bodytext,
@@ -128,11 +135,11 @@ GMEXPORT int showMessageboxYN(const char* passedHandle, const char* headline, co
     return returnVal;
 }
 
-GMEXPORT int setWindowIcon(const char* passedHandle, const char* icopath) // Currently unstable
+GMEXPORT double setWindowIcon( const char* icopath) // Currently unstable
 {   // sets an icon to the game window. Returns 1 if success and <0 if not
     // Reference: http://www.cplusplus.com/forum/windows/188709/
-    HWND handle = resolveHandle(passedHandle);
-
+    HWND handle = GetActiveWindow();
+    double returnVal = 0;
     HICON hWindowIcon=NULL;
     HICON hWindowIconBig=NULL;
     std::string stricon = icopath;
@@ -146,7 +153,8 @@ GMEXPORT int setWindowIcon(const char* passedHandle, const char* icopath) // Cur
     // set new
     if(stricon=="")
     {
-        return -1; // Stricon is empty. return.
+        return returnVal;
+        // Stricon is empty. return.
     }
     else
     {
@@ -155,11 +163,12 @@ GMEXPORT int setWindowIcon(const char* passedHandle, const char* icopath) // Cur
         SendMessage( HWND(handle), WM_SETICON, ICON_SMALL, (LPARAM)hWindowIcon );
         SendMessage( HWND(handle), WM_SETICON, ICON_BIG, (LPARAM)hWindowIconBig );
     }
-    return 1;
+    return returnVal;
 }
 
-GMEXPORT int shellExec(const char* passedHandle, const char* lpOperation, const char* lpFile)
+GMEXPORT double shellExec(const char* passedHandle, const char* lpOperation, const char* lpFile) //???
 {
+    double returnVal = 1;
     //Executes a shell command with parameters given.
     // https://docs.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutea?redirectedfrom=MSDN
     ShellExecuteA(
@@ -170,13 +179,14 @@ GMEXPORT int shellExec(const char* passedHandle, const char* lpOperation, const 
                     NULL,
                     1
                   );
-    return 1;
+    return returnVal;
 }
 
 
-GMEXPORT int shellExecEx(const char* Verb, const char* File, const char* Showtype)
+GMEXPORT double shellExecEx(const char* Verb, const char* File, const char* Showtype) // geht sicher, Showtype ist typecast zu int.
 {
     // Takes the Showtype as string : https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+    double returnVal = 1;
     ShellExecute(
                  NULL,
                  Verb, // For example "open"
@@ -185,67 +195,35 @@ GMEXPORT int shellExecEx(const char* Verb, const char* File, const char* Showtyp
                  NULL,
                  std::stoi(Showtype)
                  );
-    return 1;
+    return returnVal;
 }
 
-GMEXPORT int setFlashingHandle(const char* passedHandle, const char* flashCount, const char* flashRateMillis)
-{
-    HWND hHandle = resolveHandle(passedHandle); // Set handle to passed handle
+GMEXPORT double setFlashingSelf(double flashCount, double flashRateMillis) // geht
+{   // Sets own window to flashing
+    HWND hHandle = GetActiveWindow(); // Set handle to passed handle
     FLASHWINFO pf;                     // instanciate a struct of type flash window info
     pf.cbSize = sizeof(FLASHWINFO);
     pf.hwnd = hHandle;
     pf.dwFlags = FLASHW_TIMER|FLASHW_TRAY; // (or FLASHW_ALL to flash and if it is not minimized)
-    pf.uCount = std::stoi(flashCount);
-    pf.dwTimeout = std::stoi(flashRateMillis);
+    pf.uCount = (UINT)flashCount;
+    pf.dwTimeout = (DWORD)flashRateMillis;
 
     FlashWindowEx(&pf);
-    return 1;
+    return flashCount;
 }
 
-GMEXPORT int setFlashingName(const char* windowName, const char* cnt, const char* rate) // Doesnt take the int values .. Just ignores it
-{
+GMEXPORT double setFlashingName(const char* windowName, double flashCount, double flashRateMillis) // Geht
+{   // Takes window name and flashes it.
     HWND hHandle = FindWindow(NULL,windowName); // Set handle to passed handle
     FLASHWINFO pf;                     // instanciate a struct of type flash window info
     pf.cbSize = sizeof(FLASHWINFO);
     pf.hwnd = hHandle;
     pf.dwFlags = FLASHW_TIMER|FLASHW_TRAY; // (or FLASHW_ALL to flash and if it is not minimized)
-    pf.uCount = std::stoi(cnt);
-    pf.dwTimeout = std::stoi(rate);
+    pf.uCount = (UINT)flashCount;
+    pf.dwTimeout = (DWORD)flashRateMillis;
 
     FlashWindowEx(&pf);
-    return 1;
+    return flashCount;
 }
-
-
-GMEXPORT int setFlashingNameDefault(const char* windowName)
-{
-    HWND hHandle = FindWindow(NULL,windowName); // Set handle to passed handle
-    FLASHWINFO pf;                     // instanciate a struct of type flash window info
-    pf.cbSize = sizeof(FLASHWINFO);
-    pf.hwnd = hHandle;
-    pf.dwFlags = FLASHW_TIMER|FLASHW_TRAY; // (or FLASHW_ALL to flash and if it is not minimized)
-    pf.uCount = 8;
-    pf.dwTimeout = 80;
-
-    FlashWindowEx(&pf);
-    return 1;
-}
-
-
-
-GMEXPORT int setFlashingNormal(const char* passedHandle)
-{
-    HWND hHandle = resolveHandle(passedHandle); // Set handle to passed handle
-    FLASHWINFO pf;                     // instanciate a struct of type flash window info
-    pf.cbSize = sizeof(FLASHWINFO);
-    pf.hwnd = hHandle;
-    pf.dwFlags = FLASHW_TIMER|FLASHW_TRAY; // (or FLASHW_ALL to flash and if it is not minimized)
-    pf.uCount = 8;
-    pf.dwTimeout = 2000;
-
-    FlashWindowEx(&pf);
-    return 1;
-}
-
 
 
